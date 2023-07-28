913335625eea64c262af2cec816f6ff2d3acc18d
Nurmukhammad <sobirovn707@gmail.com>
2023-07-28 09:59:31 +0500

    url changed

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,8 +2,7 @@ import axios, { CancelToken } from "axios";
 import { logoutHandler } from "redux/reducers/authReducer";
 import { store } from "redux/rootConfig";
 
-// export const BASE_URL = "http://185.74.5.198:8000";
-export const BASE_URL = "http://10.0.1.72:8000";
+export const BASE_URL = "http://185.74.5.198:8000";
 
 export const HttpConfig = {
   BASE_URL,
--- src/pages/EditUser/index.tsx
+++ src/pages/EditUser/index.tsx
@@ -46,7 +46,6 @@ const EditUser = () => {
 
   useLayoutEffect(() => {
     userRefetch();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
